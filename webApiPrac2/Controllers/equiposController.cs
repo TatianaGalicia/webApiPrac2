d5f7f3046fac64c6c0b5225e728b961ca65b7ae4
50370 <50370@MSI>
2024-03-09 00:03:05 -0600

    Agregar archivos de proyecto.

---
+++ webApiPrac2/Controllers/equiposController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiPrac2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace webApiPrac2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+                }
+    }
+}
---
+++ webApiPrac2/Models/equiposContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace webApiPrac2.Models
+{
+    public class equiposContext: DbContext
+    {
+        public equiposContext(DbContextOptions<equiposContext> options) : base(options)
+        { 
+         
+        
+        }
+        public DbSet<equipos> equipos { get; set; }
+    }
+}
